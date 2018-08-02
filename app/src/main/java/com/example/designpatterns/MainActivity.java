eec532946ffc00d5bc177681201a51a45d1d77fb
tomorrow <1660129061@qq.com>
2018-08-02 14:25:48 +0800

    New design mode Demo 2018/8/2

--- app/src/main/java/com/example/designpatterns/MainActivity.java
+++ app/src/main/java/com/example/designpatterns/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.designpatterns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 /**
-*添加分支
+* 添加分支
+* dev更新到master
 */
 public class MainActivity extends AppCompatActivity {
 
